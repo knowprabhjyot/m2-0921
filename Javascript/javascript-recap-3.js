d177dcaeee1456f57e30ffd77ff95e3340c10a4d
Prabhjyot Gambhir <knowprabhjyot@gmail.com>
2022-03-03 07:18:42 -0800

    day 3 javascript recap

---
+++ Javascript/javascript-recap-3.js
@@ -0,0 +1,37 @@
+
+// EVENTS
+    // Parsing in HTML, CSS, Javascript
+    // defer, async keyword
+    // setTimeout
+    // setInterval
+        // clearInterval
+
+    // onclick
+    // onchangeonfocus
+    // onfocus
+    // onmouseover
+    // onmouseout
+    // onsubmit
+
+// classes
+    // Declaring a class
+    // Inheriting a class
+    // static variables
+
+
+// Promises
+    // synchronous vs asysnchronous
+    // callback functions
+    // callback hell
+    // promises
+      // states in promise
+    // then, catch
+    // async await
+    // fetch api
+
+
+// ON Tuesday *****
+// REST API
+// Localstorage, cookies, session storage
+
+
